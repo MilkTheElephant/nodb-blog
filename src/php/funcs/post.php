fe647f140093f28ac966bf8408d16a645b6f36c9
Sam Barratt <milktheelephant@gmail.com>
2014-12-16 12:48:39 +0000

    Added display specific post function post_display()

--- src/php/funcs/post.php
+++ src/php/funcs/post.php
@@ -3,10 +3,56 @@
 function posts_display($post)
 {
     include "nodb-blog/src/php/conf.php";
-    echo $post_dir."/".$post;
-    echo file_get_contents($post_dir."/".$post);
-    echo "Skeleton function to display a specific post";
+    echo "<style>
+        .date {
+            text-align: left;
+            width: 49%;
+            float: left;
+            }
+        .tags {
+            text-align: right;
+            width: 49%;
+            float: right;
+            }
+        </style>";
+
+
+    echo '<div style="width: 100%;">';
+    echo "<div class='date'>";
+    if ($show_dates == True) //show the dates if specified.
+    {
+        echo "<i style='text-align:left;'>".date($date_format,intval(substr($post, 0, strpos($post, "_"))))."</i><br>";
+    }
+    echo "</div>";
+    echo "<div class='tags'>"; //Show the post's tags if specified.
+    if ($show_tags  == True)
+    {
+        $tagfile = $post_dir."/.".$post."_tags";
+        $tags = fopen($tagfile, "r");
+        if ($tags == FALSE)
+        {
+            echo 'ERROR: No tags file';
+        }
+        else
+        {
+            while (($line = fgets($tags)) !== FALSE )
+            {
+                echo "<a href='/sbarratt/blog.php?tag=".$line."'>#".$line."</a>";
+            }
+        }
+    }
+    echo "</div></div><br>";
+    $out = file_get_contents($post_dir."/".$post); //Get post. TODO replace this with getting specific lines so that we can use plain text files instead of  HTML>
+    if ($out == FALSE)
+    {
+        echo "ERROR: Cannot find file:".$dir."/".$post."";
+    }
+    else
+    {
+        echo $out;
+    }
+    
     return "";
-}
+    }
 
 ?>
